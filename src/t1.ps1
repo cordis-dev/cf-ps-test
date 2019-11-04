8045bd87e6bdeeede97efa7570ba9cf959a20ba2
cordis-dev <darius@adroiti.com>
2019-11-04 15:37:49 +0200

    Create t1.ps1

---
+++ src/t1.ps1
@@ -0,0 +1,29 @@
+Describe 'Testing against PSSA rules' {
+
+    Context 'PSSA Standard Rules' {
+
+        $analysis = Invoke - ScriptAnalyzer - Path '.\t.ps1'
+        $scriptAnalyzerRules = Get - ScriptAnalyzerRule
+
+        forEach($rule in $scriptAnalyzerRules) {
+
+            It "Should pass $rule" {
+
+                If($analysis.RuleName - contains $rule) {
+
+                    $analysis |
+
+                        Where RuleName - EQ $rule - outvariable failures |
+                        Out - Default
+
+                    $failures.Count | Should Be 0
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
